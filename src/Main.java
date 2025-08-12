1cbdc0b28a3f3fc2eb393e47dfcb47185cfe59b0
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-08-12 12:07:20 +0200

    6 - On déménage dynamiquement

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,52 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Le hello de geraud");
         System.out.println("Je vais taider à déménager !!!");
-
-        int nbTotalCartons = 34;
         int nbVoyages = 0;
+        int nbTotalCartons = 0;
+        int truckCapacity = 0;
+        boolean isNumbernbTotalCartons = false;
+        boolean isNumbertruckCapacity = false;
+
+        while (!isNumbernbTotalCartons) {
+            System.out.println("Combien de cartons avez vous ?");
+            if(input.hasNextInt()) {
+                nbTotalCartons = input.nextInt();
+                if(nbTotalCartons > 0) {
+                    isNumbernbTotalCartons = true;
+                }else{
+                    System.out.println("Le nombre doit être supérieur à O !");
+                }
+            }else{
+                System.out.println("Ce n'est pas un nombre. Réessayez !");
+                input.next();
+            }
+        }
+        while(!isNumbertruckCapacity) {
+            System.out.println("Combien de cartons on peut mettre dans votre camion ?");
+            if(input.hasNextInt()) {
+                truckCapacity = input.nextInt();
+                if(truckCapacity > 0) {
+                    isNumbertruckCapacity = true;
+                }else{
+                    System.out.println("Le nombre doit être supérieur à O !");
+                }
+            }else{
+                System.out.println("Ce n'est pas un nombre. Réessayez !");
+                input.next();
+            }
+        }
+        input.close();
 
         while(nbTotalCartons > 0){
-            if(nbTotalCartons >= 9){
-                nbTotalCartons-=9;
-                System.out.println("Un voyage de 9 cartons");
+            if(nbTotalCartons >= truckCapacity){
+                nbTotalCartons-=truckCapacity;
+                System.out.println("Un voyage de " + truckCapacity + " cartons");
             }else {
                 System.out.println("Un voyage de " + nbTotalCartons + " cartons");
                 nbTotalCartons-=nbTotalCartons;
